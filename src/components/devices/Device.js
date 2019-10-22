5f2f2f770fd9d7841ba70df6f058d8c60c77ad62
Federico Rufrancos <federico.rufrancos@fluxit.com.ar>
2019-10-22 14:44:37 -0300

    Device assignation flow complete

--- src/components/devices/Device.js
+++ src/components/devices/Device.js
@@ -4,8 +4,11 @@ import { ReactComponent as Info } from '../../assets/info-circle-solid.svg';
 
 const Device = ({ device }) => {
   return (
-    <tr className={device.assignedTo ? 'table-danger' : 'table-dark'}>
-      <th scope="row">{device.name}</th><td>{device.assignedTo}</td>
+    <tr
+      className={device.assignedTo.fluxerName ? 'table-danger' : 'table-dark'}
+    >
+      <th scope="row">{device.name}</th>
+      <td>{device.assignedTo.fluxerName}</td>
       <td>{device.OSVersion}</td>
       <td>
         <Link to={`/device/${device.id}`}>
--- src/components/devices/DeviceInfo.js
+++ src/components/devices/DeviceInfo.js
@@ -1,47 +1,107 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { ReactComponent as ArrowLeft } from "../../assets/arrow-circle-left-solid.svg";
-import Swal from "sweetalert2";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as ArrowLeft } from '../../assets/arrow-circle-left-solid.svg';
+import Swal from 'sweetalert2';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 
 const DeviceInfo = ({ match, history, firestore, deviceInfo }) => {
-  
   if (!deviceInfo) {
     return <h1> Esperando </h1>;
   }
 
-  let operation = "Reservar";
-  let classNameOperation = "btn-danger";
-  let nextAssignTo = 'Federico Rufrancos';
-  let alerOpt = ["Reservado!", "El dispositivo te fue asignado.", "success"];
+  let operation = 'Reservar';
+  let classNameOperation = 'btn-danger';
+  let alerOpt = ['Reservado!', 'El dispositivo te fue asignado.', 'success'];
+  let operationStatus = 'assign';
   if (
     deviceInfo.assignedTo !== '' &&
-    deviceInfo.assignedTo === 'Federico Rufrancos'
+    deviceInfo.assignedTo.fluxerName === 'Federico Rufrancos'
   ) {
     classNameOperation = 'btn-success';
     operation = 'Liberar dispositivo';
-    alerOpt = ["Liberado!", "Gracias por liberar el dispositivo.", "success"]
-    nextAssignTo = '';
-  } else if (deviceInfo.assignedTo !== "") {
+    alerOpt = ['Liberado!', 'Gracias por liberar el dispositivo.', 'success'];
+    operationStatus = 'release';
+  } else if (deviceInfo.assignedTo !== '') {
     classNameOperation = 'btn-warning';
     operation = 'Solicitar dispositivo';
-
+    operationStatus = 'pending';
   }
-
-
 
   const onUpdateDeviceAssignment = () => {
     const { id } = match.params;
-    const deviceUpdated = {...deviceInfo, assignedTo : nextAssignTo};
-    firestore.update({
-      collection: 'devices',
-      doc: id
-    }, deviceUpdated).then(() => {
-      Swal.fire(...alerOpt);
-      history.push("/");
-    });
+    let newAssignation = '';
+    let waitingList = deviceInfo.waitingList ? deviceInfo.waitingList : [];
+    let newAssignationRecord = '';
+    switch (operationStatus) {
+      case 'assign':
+        newAssignation = {
+          fluxerId: 1111,
+          fluxerName: 'Federico Rufrancos',
+          dateAssigned: new Date().toLocaleDateString()
+        };
+        break;
+      case 'pending':
+        newAssignation = deviceInfo.assignedTo;
+        waitingList.push({
+          fluxerId: 1111,
+          fluxerName: 'Federico Rufrancos',
+          dateRequest: new Date().toLocaleDateString()
+        });
+        break;
+      case 'release':
+        debugger;
+        newAssignationRecord = { ...deviceInfo.assignedTo };
+        newAssignationRecord.dateRelease = new Date().toLocaleDateString();
+        newAssignationRecord.deviceId = id;
+        deviceInfo.assignedTo = '';
+        if (waitingList.length > 0) {
+          newAssignation = waitingList.pop();
+          newAssignation.dateAssigned = new Date().toLocaleDateString();
+          deviceInfo.assignedTo = newAssignation;
+          alerOpt = [
+            'Reasignado!',
+            'El dispositivo fue asignado al proximos Fluxer.',
+            'success'
+          ];
+        }
+        break;
+      default:
+        break;
+    }
+
+    const deviceUpdated = {
+      ...deviceInfo,
+      assignedTo: newAssignation,
+      waitingList
+    };
+    firestore
+      .update(
+        {
+          collection: 'devices',
+          doc: id
+        },
+        deviceUpdated
+      )
+      .then(() => {
+        if (newAssignationRecord) {
+          console.log('newAssignationRecord ', newAssignationRecord);
+          firestore
+            .add({ collection: 'assignations' }, newAssignationRecord)
+            .then(() => {
+              Swal.fire(...alerOpt);
+              console.log('redirect');
+              history.push('/');
+            });
+        } else {
+          Swal.fire(...alerOpt);
+          history.push('/');
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   return (
@@ -61,7 +121,7 @@ const DeviceInfo = ({ match, history, firestore, deviceInfo }) => {
             {deviceInfo.assignedTo ? (
               <p className="card-text">
                 <span className="font-weight-bold"> Asignado a : </span>
-                {deviceInfo.assignedTo}
+                {deviceInfo.assignedTo.fluxerName}
               </p>
             ) : null}
             <p className="card-text">
@@ -103,12 +163,14 @@ const DeviceInfo = ({ match, history, firestore, deviceInfo }) => {
 };
 
 export default compose(
-  firestoreConnect(props => [{ 
-    collection: 'devices',
-    storeAs: 'device',
-    doc: props.match.params.id
-  }]),
-  connect(({firestore: {data}}, props) => ({ deviceInfo: data.device && data.device}))
+  firestoreConnect(props => [
+    {
+      collection: 'devices',
+      storeAs: 'device',
+      doc: props.match.params.id
+    }
+  ]),
+  connect(({ firestore: { data } }, props) => ({
+    deviceInfo: data.device && data.device
+  }))
 )(DeviceInfo);
-
-
