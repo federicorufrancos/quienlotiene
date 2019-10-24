183c78856b6eb72b758b90996c3018776fc66ed9
Federico Rufrancos <federico.rufrancos@fluxit.com.ar>
2019-10-24 19:41:32 -0300

    Fix new device page styling

--- src/components/devices/NewDevice.js
+++ src/components/devices/NewDevice.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import Header from '../layouts/Header';
+import { Link } from 'react-router-dom';
+import { ReactComponent as ArrowLeft } from '../../assets/arrow-circle-left-solid.svg';
 import { firestoreConnect } from 'react-redux-firebase';
 import PropTypes from 'prop-types';
 import Swal from 'sweetalert2';
@@ -44,120 +45,128 @@ const NewDevice = ({ history, firestore }) => {
 
   return (
     <div className="col-lg-12">
-      <div className="row ml-3">
-        <Header titulo="Agregar dispositivo" />
-      </div>
-      <form className="card border-light p-3" onSubmit={onSubmitDevice}>
-        <div className="form-group">
-          <label> Nombre: </label>
-          <input
-            type="text"
-            name={name}
-            onChange={e => saveName(e.target.value)}
-            placeholder="Escriba el nombre del dispositivo"
-            required
-            className="form-control"
-          />
+      <Link to="/" className="btn btn-info">
+        <ArrowLeft className="app-icon p-1" />
+        Volver a la info
+      </Link>
+      <div className="card card-device-info border-secondary mb-3 mt-3">
+        <div className="card-header">
+          <span className="lead">Agregar dispositivo</span>
         </div>
-        <div className="row">
-          <div className="col-lg-6">
+        <form onSubmit={onSubmitDevice}>
+          <div className="card-body">
             <div className="form-group">
-              <div className="form-group">
-                <label> Sistema Operativo </label>
-                <select
-                  className="form-control"
-                  name={operativeSystem}
-                  onChange={e => saveOperativeSystem(e.target.value)}
-                >
-                  <option option=""> Seleccione el sistema operativo </option>
-                  {operativeSystems.map((operativeSystem, index) => (
-                    <option key={index} value={operativeSystem}>
-                      {operativeSystem}
-                    </option>
-                  ))}
-                </select>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-6">
-            <div className="form-group">
-              <label> Versión: </label>
+              <label> Nombre: </label>
               <input
                 type="text"
-                name={OSVersion}
-                onChange={e => saveOSVersion(e.target.value)}
-                placeholder="Escriba la versión del SO"
+                name={name}
+                onChange={e => saveName(e.target.value)}
+                placeholder="Escriba el nombre del dispositivo"
                 required
                 className="form-control"
               />
             </div>
-          </div>
-        </div>
-        <div className="form-group">
-          <label> Id de Flux: </label>
-          <input
-            type="text"
-            name={IDFlux}
-            onChange={e => saveIDFlux(e.target.value)}
-            placeholder="Escriba el Id de Flux"
-            required
-            className="form-control"
-          />
-        </div>
-        <div className="row">
-          <div className="col-lg-6">
+            <div className="row">
+              <div className="col-lg-6">
+                <div className="form-group">
+                  <div className="form-group">
+                    <label> Sistema Operativo </label>
+                    <select
+                      className="form-control"
+                      name={operativeSystem}
+                      onChange={e => saveOperativeSystem(e.target.value)}
+                    >
+                      <option option="">Seleccione el sistema operativo</option>
+                      {operativeSystems.map((operativeSystem, index) => (
+                        <option key={index} value={operativeSystem}>
+                          {operativeSystem}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="form-group">
+                  <label> Versión: </label>
+                  <input
+                    type="text"
+                    name={OSVersion}
+                    onChange={e => saveOSVersion(e.target.value)}
+                    placeholder="Escriba la versión del SO"
+                    required
+                    className="form-control"
+                  />
+                </div>
+              </div>
+            </div>
             <div className="form-group">
-              <label> IMEI: </label>
+              <label> Id de Flux: </label>
               <input
                 type="text"
-                name={IMEI}
-                onChange={e => saveIMEI(e.target.value)}
-                placeholder="Escriba el código de IMEI"
+                name={IDFlux}
+                onChange={e => saveIDFlux(e.target.value)}
+                placeholder="Escriba el Id de Flux"
+                required
                 className="form-control"
-                disabled={deviceId ? true : false}
               />
             </div>
-          </div>
-          <div className="col-lg-6">
+            <div className="row">
+              <div className="col-lg-6">
+                <div className="form-group">
+                  <label> IMEI: </label>
+                  <input
+                    type="text"
+                    name={IMEI}
+                    onChange={e => saveIMEI(e.target.value)}
+                    placeholder="Escriba el código de IMEI"
+                    className="form-control"
+                    disabled={deviceId ? true : false}
+                  />
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="form-group">
+                  <label> Device ID: </label>
+                  <input
+                    type="text"
+                    name={deviceId}
+                    onChange={e => saveDeviceId(e.target.value)}
+                    placeholder="Escriba Id dispositivo IOS"
+                    className="form-control"
+                    disabled={IMEI ? true : false}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="form-group mb-2">
+              <label>
+                Funciona:
+                <input
+                  className="form-check-input ml-3 mt-2"
+                  type="checkbox"
+                  checked={works}
+                  onChange={e => saveWorks(e.target.checked)}
+                />
+              </label>
+            </div>
             <div className="form-group">
-              <label> Device ID: </label>
-              <input
-                type="text"
-                name={deviceId}
-                onChange={e => saveDeviceId(e.target.value)}
-                placeholder="Escriba Id dispositivo IOS"
+              <label> Comentarios: </label>
+              <textarea
+                name={comments}
+                placeholder="Escriba algunos comentarios"
                 className="form-control"
-                disabled={IMEI ? true : false}
-              />
+                onChange={e => saveComments(e.target.value)}
+              ></textarea>
             </div>
-          </div>
-        </div>
-        <div className="form-group mb-2">
-          <label>
-            Funciona:
             <input
-              className="form-check-input ml-3 mt-2"
-              type="checkbox"
-              checked={works}
-              onChange={e => saveWorks(e.target.checked)}
+              type="submit"
+              value="Agergar dispositivo"
+              className="btn btn-success"
             />
-          </label>
-        </div>
-        <div className="form-group">
-          <label> Comentarios: </label>
-          <textarea
-            name={comments}
-            placeholder="Escriba algunos comentarios"
-            className="form-control"
-            onChange={e => saveComments(e.target.value)}
-          ></textarea>
-        </div>
-        <input
-          type="submit"
-          value="Agergar dispositivo"
-          className="btn btn-success"
-        />
-      </form>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
